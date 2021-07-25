acc28311b262adebbad3f1ddf111b79b214ab8e9
Olga Seledets <olga.seledets@outlook.com>
2021-07-25 19:17:57 +0300

    Assign values to variables. Add console output using methods and properties

--- src/script.js
+++ src/script.js
@@ -1,9 +1,20 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 270;
+let income = 'Стипендия';
+let addExpenses = 'Интернет, еда, проездной';
+let deposit = false;
+let mission = 2000;
+let period = 12;
+let budgetDay = money / 30;
 
-alert('Calculator');
-console.log('Calculator');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log('Дневной бюджет равен ' + budgetDay);
