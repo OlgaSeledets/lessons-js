e18608ff790d9538cd9e77c1273e5a8ffbeb0dc6
Olga Seledets <olga.seledets@outlook.com>
2021-07-28 19:30:31 +0300

    Add getString function

--- src/hard_task.js
+++ src/hard_task.js
@@ -31,3 +31,17 @@ let namePerson = prompt('Введите имя пользователя');
 let result = namePerson === 'Артём' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент';
 
 console.log(result);
+
+const text = '         Это моя строка Это моя строка       ';
+
+const getString = function(text) {
+    if (typeof(text) === 'number') {
+        return ('Это не строка');
+    } else if (text.length > 30) {
+        return (text.trim().slice(0, 30) + '...');
+    } else {
+        return text.trim();
+    }
+}
+
+console.log(getString(text));
